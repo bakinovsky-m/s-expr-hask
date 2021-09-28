bba4dd232a2fbc73322fcf3aa2146f6958163ae3
bakinovsky-m <makbak@ya.ru>
2021-09-28 19:18:14 +0300

    hspec tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,20 @@
+import Test.Hspec
 import Lib
 
 
 main :: IO ()
-main = putStrLn "qwe"
+main = hspec $ do
+  describe "eval" $ do
+    it "evaluate value s-expr" $
+      (eval . SValue $ Value 123) `shouldBe` 123
+
+    it "evaluate recursive s-expr" $
+      eval (SOp Plus (SValue $ Value 3) (SValue $ Value 1)) `shouldBe` 4
+
+  describe "parse_and_eval" $ do
+    it "parses valid value s-expr" $
+      parse_and_eval sexpr "123" `shouldBe` Right 123
+    it "parses valid recursive s-expr" $
+      parse_and_eval sexpr "(+ 123 321)" `shouldBe` Right (123 + 321)
+    it "fails on invalid strings" $
+      parse_and_eval sexpr "(" `shouldSatisfy` either (const True) (const False)
